e716d372cc858cbfb2cf28eee0a1c397843ff31f
Shyama Pada Mondal <shyamapadamondal30@gmail.com>
2023-12-19 22:18:10 +0530

    Route and Pagges added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-export default function App() {
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+
+
+function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path = '/' element = {<Home />} />
+      <Route path = '/log-in' element = {<LogIn />} />
+      <Route path = '/sign-up' element = {<SignUp />} />
+      <Route Path = '/about' element = {<About />} />
+      <Route path = '/profile' element = {<Profile />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App;
