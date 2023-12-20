98919d8bbd34561219de2c6d36f2b4660b7bc7a5
Shyama Pada Mondal <shyamapadamondal30@gmail.com>
2023-12-20 16:19:35 +0530

    update the header.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,13 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path = '/' element = {<Home />} />
       <Route path = '/log-in' element = {<LogIn />} />
