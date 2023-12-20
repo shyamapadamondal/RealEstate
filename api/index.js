e58d2666cdaa7ec37f6a45624792b326298167c0
Shyama Pada Mondal <shyamapadamondal30@gmail.com>
2023-12-21 01:57:39 +0530

    mongodb server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//Database Connection ............
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log('Connected to the Mongo DB');
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log('Server is runningg on port 3000!');
-}); 
+//Server Connection .............
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+});
